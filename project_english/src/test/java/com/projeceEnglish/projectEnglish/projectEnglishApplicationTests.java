0d2e37f7ac4090555a11b372af791eebacd15c14
DragonAbyss <80340310+Abyss-of-Dragon@users.noreply.github.com>
2021-06-08 16:07:26 +0800

    jetbrains-toolbox

---
+++ project_english/src/test/java/com/projeceEnglish/projectEnglish/projectEnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeceEnglish.projectEnglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class projectEnglishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
